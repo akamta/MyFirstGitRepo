8491b70a497b97d130a4c4dfb211531039412f2d
Andrew Kamta <andrewkamta47@hotmail.com>
2020-05-27 15:31:18 +0000

    this is my third commit

--- MyNameIsAndrew.java
+++ MyNameIsAndrew.java
@@ -1,5 +1,5 @@
 public class MyNameIsAndrew {
 	public static void main(String[] args){
-		System.out.println("My Favorite Programming language is Linux");
+		System.out.println("I am taking Cs407 because I want to graduate Dec 2020");
 	}
 }
